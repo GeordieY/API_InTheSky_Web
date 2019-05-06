9b6c9c8b4baa1d6cea1731aa2cf326665c271993
Charles Cunningham <mureytasroc@gmail.com>
2019-05-06 17:31:40 -0400

    m

--- public/scripts/search-p5.js
+++ public/scripts/search-p5.js
@@ -4,6 +4,16 @@ var startX = 0;
 var y = -200;
 var startY = 0;
 
+const key = 'AIzaSyBrhDK19qy0m3Vfg7CGPwF47c5RgmbmcA0';
+const options = {
+  lat: -0.77176,
+  lng: 11.689,
+  zoom: 3,
+};
+const mappa = new Mappa('Google', key);
+let myMap;
+let meteorites;
+
 function preload() {
 	img = loadImage("images/map.png");
 }
@@ -14,13 +24,32 @@ function setup() {
     // Move the canvas so it’s inside our <div id="sketch-holder">.
     canvas.parent('sketch-holder');
 
+
+//mappa==============
+		myMap = mappa.tileMap(options);
+myMap.overlay(canvas);
+
+// Load the data
+meteorites = loadTable('data/Meteorite_Landings.csv', 'csv', 'header');
+
+// Only redraw the meteorites when the map change and not every frame.
+myMap.onChange(drawMeteorites);
+
+fill(207, 204, 0);
+noStroke();
+//======================
+
+
     background(230);
 }
 
 function draw() {
-  background(230);
+  //background(230);
   //ellipse(50, 50, 80, 80);
-  image(img, x, y, 1416, 1206);
+  //image(img, x, y, 1416, 1206);
+
+
+
 }
 
 function mousePressed() {
@@ -35,4 +64,29 @@ function mouseDragged() {
   diff = startY - mouseY;
   y = y - diff;
   startY = mouseY;
+}
+
+function drawMeteorites() {
+  // Clear the canvas
+  clear();
+
+  for (let i = 0; i < meteorites.getRowCount(); i += 1) {
+    // Get the lat/lng of each meteorite
+    const latitude = Number(meteorites.getString(i, 'reclat'));
+    const longitude = Number(meteorites.getString(i, 'reclong'));
+
+    // Only draw them if the position is inside the current map bounds. We use a
+    // Google Map method to check if the lat and lng are contain inside the current
+    // map. This way we draw just what we are going to see and not everything. See
+    // getBounds() in https://developers.google.com/maps/documentation/javascript/3.exp/reference
+    if (myMap.map.getBounds().contains({ lat: latitude, lng: longitude })) {
+      // Transform lat/lng to pixel position
+      const pos = myMap.latLngToPixel(latitude, longitude);
+      // Get the size of the meteorite and map it. 60000000 is the mass of the largest
+      // meteorite (https://en.wikipedia.org/wiki/Hoba_meteorite)
+      let size = meteorites.getString(i, 'mass (g)');
+      size = map(size, 558, 60000000, 1, 25) + myMap.zoom();
+      ellipse(pos.x, pos.y, size, size);
+    }
+  }
 }
