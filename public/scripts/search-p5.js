0c21246e7b681c44f38f86726901267d85cfd8c2
Charles Cunningham <mureytasroc@gmail.com>
2019-05-09 13:41:35 -0400

    m

--- public/scripts/search-p5.js
+++ public/scripts/search-p5.js
@@ -6,48 +6,84 @@ var startY = 0;
 
 const key = 'AIzaSyBrhDK19qy0m3Vfg7CGPwF47c5RgmbmcA0';
 const options = {
-  lat: -0.77176,
-  lng: 11.689,
-  zoom: 3,
+  lat: 40.7128,
+  lng: -74.0060,
+  zoom: 10.25,
 };
 const mappa = new Mappa('Google', key);
 let myMap;
 let meteorites;
 
+var selectPoint=true;
+
 function preload() {
 	img = loadImage("images/map.png");
 }
 
+let canvas;
+
 function setup() {
-  var canvas = createCanvas(1500, 500);
+  canvas = createCanvas(windowWidth, 500);
 
     // Move the canvas so it’s inside our <div id="sketch-holder">.
     canvas.parent('sketch-holder');
 
+document.getElementById("searchbc").addEventListener("click",search);
 
 //mappa==============
 		myMap = mappa.tileMap(options);
 myMap.overlay(canvas);
 
+
 // Load the data
-meteorites = loadTable('data/Meteorite_Landings.csv', 'csv', 'header');
+//meteorites = loadTable('data/Meteorite_Landings.csv', 'csv', 'header');
 
 // Only redraw the meteorites when the map change and not every frame.
-myMap.onChange(drawMeteorites);
+//myMap.onChange(drawMeteorites);
 
 fill(207, 204, 0);
 noStroke();
 //======================
 
 
-    background(230);
+    //background(230);
+}
+
+
+function search(){
+  var input = document.getElementById("searchterm").value;
+  document.getElementById("searchterm").value="";
+  console.log(input);
+  myMap.setOptions({draggable: false});
+}
+
+function windowResized() {
+  /*canvas = createCanvas(windowWidth, 500);
+
+    // Move the canvas so it’s inside our <div id="sketch-holder">.
+    canvas.parent('sketch-holder');
+
+
+//mappa==============
+myMap.overlay(canvas);
+
+myMap.checkResize();*/
+
 }
 
 function draw() {
   //background(230);
   //ellipse(50, 50, 80, 80);
   //image(img, x, y, 1416, 1206);
-
+  if(mouseX<width&&mouseX>0&&mouseY>0&&mouseY<height){
+    if(selectPoint){
+      //document.body.style.cursor="default";
+    //console.log("hello");
+  }
+  }
+  else{
+    //document.body.style.cursor="grab";
+  }
 
 
 }
